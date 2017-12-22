461a5823d45567211576981246d20c9ed4fb3a75
tangyangyang <tangyang22007@126.com>
2017-12-22 17:55:44 +0800

    删掉多余代码

--- RNApp/App.js
+++ RNApp/App.js
@@ -10,40 +10,8 @@ import MainScreen from './components/mainScreen'
 import homePage from './components/app'
 import aboutScreen from './components/about'
 import {
-  StackNavigator,
-  TabNavigator,
   DrawerNavigator
 } from 'react-navigation'
-
-// const App = StackNavigator({
-//   Home: {
-//     screen: homePage,
-//     navigationOptions: {
-//       title: '首页'
-//     }
-//   },
-//   Main: {
-//     screen: MainScreen,
-//     navigationOptions: {
-//       title: '第二个页面'
-//     }
-//   }
-// })
-
-// const App = TabNavigator({
-//   Home: {
-//     screen: homePage
-//   },
-//   Main: {
-//     screen: MainScreen
-//   }
-// }, {
-//   tabBarPosition: 'top',
-//   animationEnabled: true,
-//   tabBarOptions: {
-//     activeTintColor: '#e91e63'
-//   }
-// })
 
 const App = DrawerNavigator({
   Home: {
